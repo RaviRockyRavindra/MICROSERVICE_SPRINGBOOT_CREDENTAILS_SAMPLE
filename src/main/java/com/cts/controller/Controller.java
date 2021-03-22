f6da6f0cbadb27a15deeb6518ebcdeed4711fcae
K.Ravindra sai <37626998+RaviRockyRavindra@users.noreply.github.com>
2021-03-22 12:29:43 +0530

    Update Controller.java

--- src/main/java/com/cts/controller/Controller.java
+++ src/main/java/com/cts/controller/Controller.java
@@ -1,7 +1,7 @@
 package com.cts.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,104 +18,16 @@ public class Controller
 	
 	String ERROR="errors";
 	
-	@Autowired
-	UserCrudRepo usercrudrepo;
 	
-	@Autowired
-	SellerCrudRepo sellercrudrepo;
-	
-	@PostMapping(value="/buyerregister")
-	public String BRegister(@RequestParam String Username, @RequestParam String password,@RequestParam String role) 
+	@GetMapping(value="/one")
+	public String BRegister() 
 	{
 	
-		Emart_Users_Buyers users = new Emart_Users_Buyers();
-		
-		users.setUsername(Username);
-		
-		users.setPassword(password);
-		
-		users.setRole(role);
-		
-		usercrudrepo.save(users);
-		
 		return SUCCESS;
 		
 	}
 	
-	@PostMapping(value="/buyerlogin")
-	public String BLogin(@RequestParam String username, @RequestParam String password) 
-	{
 	
-		
-		Emart_Users_Buyers hh=usercrudrepo.findByusername(username);
-		
-		
-			if(hh.getPassword().toString().equals(password))
-			{
-				return SUCCESS;
-			}
-			else
-			{
-				return "wrong password";
-			}
-			
-		
-	}
-	
-	@PostMapping(value="/sellerregister")
-	public String SRegister(@RequestParam String Username, @RequestParam String password,@RequestParam String role) 
-	{
-	
-		Emart_Users_Sellers users = new Emart_Users_Sellers();
-		
-		users.setUsername(Username);
-		
-		users.setPassword(password);
-		
-		users.setRole(role);
-		
-		sellercrudrepo.save(users);
-		
-		return SUCCESS;
-		
-	}
-	
-	@PostMapping(value="/sellerlogin")
-	public String SLogin(@RequestParam String username, @RequestParam String password) 
-	{
-	
-	
-		Emart_Users_Sellers users = sellercrudrepo.findByusername(username);
-	
-	
-		
-		if(sellercrudrepo.findByusername(username)!=null)
-		{
-			
-			if(users.getPassword().toString().equals(password))
-			{
-				return SUCCESS;
-			}
-			else
-			{
-				return ERROR;
-			}
-			
-			
-			
-		}
-		
-		else
-		{
-			
-			return ERROR;
-			
-		}
-		
-
-
-	
-	}
 	
 	
 }
