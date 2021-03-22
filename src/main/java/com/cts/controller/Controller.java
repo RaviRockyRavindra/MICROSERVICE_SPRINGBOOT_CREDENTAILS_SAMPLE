b64f89664c439d2a113ef7f351e4b74acc160bf3
K.Ravindra sai <37626998+RaviRockyRavindra@users.noreply.github.com>
2021-03-22 12:39:24 +0530

    Update Controller.java

--- src/main/java/com/cts/controller/Controller.java
+++ src/main/java/com/cts/controller/Controller.java
@@ -5,10 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cts.entity.Emart_Users_Buyers;
-import com.cts.entity.Emart_Users_Sellers;
-import com.cts.repo.SellerCrudRepo;
-import com.cts.repo.UserCrudRepo;
+
 
 @RestController
 public class Controller 
